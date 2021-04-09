bb46eebcbb2e576f5a519b202a989ffc53a91abd
PaulStoffregen <paul@pjrc.com>
2021-04-09 14:00:19 -0700

    Default CS pin and comments in example

--- TMM-HM01B0/examples/hm01b0_test/hm01b0_test.ino
+++ TMM-HM01B0/examples/hm01b0_test/hm01b0_test.ino
@@ -32,9 +32,9 @@ HM01B0 hm01b0;
 //#define USE_SDCARD 1
 File file;
 
-#define TFT_DC  1
-#define TFT_CS  5
-#define TFT_RST  0
+#define TFT_DC  1   // "TX1" on left side of Sparkfun ML Carrier
+#define TFT_CS  4   // "CS" on left side of Sparkfun ML Carrier
+#define TFT_RST  0  // "RX1" on left side of Sparkfun ML Carrier
 
 //#define TFT_ST7789 1
 #define TFT_ILI9341 1
@@ -50,7 +50,7 @@ ST7789_t3 tft = ST7789_t3(TFT_CS, TFT_DC, TFT_RST);
 #define TFT_BLUE  ST77XX_BLUE
 
 #else
-#include "ILI9341_t3n.h"
+#include "ILI9341_t3n.h" // https://github.com/KurtE/ILI9341_t3n
 ILI9341_t3n tft = ILI9341_t3n(TFT_CS, TFT_DC, TFT_RST);
 #define TFT_BLACK ILI9341_BLACK
 #define TFT_YELLOW ILI9341_YELLOW
